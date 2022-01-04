d8a6e3ab098c4ae99d52d0160769084687aaafd3
shaikyasmin6 <shaikyasminsulthana28@gmail.com>
2022-01-04 09:31:00 +0530

    aboutus done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ExamsComponent } from './exams/exams.component';
+import { NewsComponent } from './news/news.component';
+import { FaqComponent } from './faq/faq.component';
+import { AppRoutingModule } from './app-routing.module';
+import { InterComponent } from './inter/inter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    ExamsComponent,
+    NewsComponent,
+    FaqComponent,
+    InterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
